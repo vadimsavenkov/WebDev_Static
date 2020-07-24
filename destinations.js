d363846618f0d81f3bd8231f2b93a53deb2ea298
vadimsavenkov <vadim.y.savenkov@gmail.com>
2020-07-23 21:57:03 -0600

    gallery tweaks

--- destinations.js
+++ destinations.js
@@ -4,42 +4,48 @@ const destinations = [
     title: "Europe",
     url: 'https://unsplash.com/photos/t2hgHV1R7_g',
     filename: 'Canal',
-    place: 'Netherlands'
+    place: 'Amsterdam, Netherlands',
+    text: 'Amsterdam, capital of the Netherlands, has more than one hundred kilometers of grachten(canals), about 90 islands and 1, 500 bridges.The three main canals(Herengracht, Prinsengracht and Keizersgracht), dug in the 17th century during the Dutch Golden Age, form concentric belts around the city, known as the Grachtengordel.Alongside the main canals are 1550 monumental buildings.The 17th - century canal ring area, including the Prinsengracht, Keizersgracht, Herengracht and Jordaan, were listed as UNESCO World Heritage Site in 2010, contributing to Amsterdam as the Venice of the North'
   },
   {
     id: 174,
     title: "Asia Pacific",
-    url: 'https://unsplash.com/photos/n7DY58YFg9E',
-    filename: 'Rice Terrace', 
-    place: 'Indonesia'
+    url: 'https://unsplash.com/photos/XEhchWQuWyM',
+    filename: 'Tegallalang Rice Terrace', 
+    place: 'Bali, Indonesia',
+    text:'Tegalalang Rice Terrace is one of the famous tourist objects in Bali situated in Tegalalang Village north of Ubud Bali featured by the amazing rice terrace set the cliff. Tegalalang Village is located on 600 m above sea levels covered by good temperature where most of the villagers as a farmer. It offers the stunning view of rice terrace where we can see from the main road from Kintamani to Ubud. This rice terrace is one of the alternative tourist destinations to visit during Kintamani Tour or Ubud Village tour'
   },
   {
     id: 482,
     title: 'North America',
     url: 'https://unsplash.com/photos/JZKGkj46bwE',
     filename: 'Mountains', 
-    place: 'Canada'
+    place: 'Yoho National Park, Canada',
+    text:'Yoho National Park is a national park of Canada in British Columbia. It is located in the Rocky Mountains along the western slope of the Continental Divide of the Americas in southeastern British Columbia. The word Yoho is a Cree expression of amazement or awe, and it is an apt description for the spectacular landscape of massive ice fields and mountain peaks that rank among the highest in the Canadian Rockies'
   },
   {
     id: 645,
     title:'South America',
     url: 'https://unsplash.com/photos/GFOnOrv-t6w',
-    filename: 'Machu-Pichu',
-    place: 'Peru'
-  },
+    filename: 'Inca Citadel',
+    place: 'Machu-Pichu, Peru',
+    text: 'Machu Picchu is a 15th-century Inca citadel, located in the Eastern Cordillera of southern Peru, on a 2,430-metre (7,970 ft) mountain ridge.It is located in the Cusco Region, Urubamba Province, Machupicchu District,[4] above the Sacred Valley, which is 80 kilometres (50 mi) northwest of Cuzco. The Urubamba River flows past it, cutting through the Cordillera and creating a canyon with a tropical mountain climate'
+  },  
   {
     id: 525,
     title:'North Africa',
     url: 'https://unsplash.com/photos/_VqyrvQi6do',
     filename: 'Desert',
-    place: 'Morocco'
+    place: 'Sahara, Morocco',
+    text:'Morocco (the place the sun sets) is a country located in the Maghreb region of North Africa. It overlooks the Mediterranean Sea to the north and the Atlantic Ocean to the west, with land borders with Algeria to the east and Western Sahara (status disputed) to the south. Morocco also claims the exclaves of Ceuta, Melilla and Peñón de Vélez de la Gomera, all of them under Spanish jurisdiction, as well as several small Spanish-controlled islands off its coast'
   },
   {
     id: 643,
     title:'Australia',
     url: 'https://unsplash.com/photos/Pa0AqyCLeec',
-    filename: 'Serfing',
-    place: 'New South Wales'
+    filename: 'Surfing',
+    place: 'Sydney, Bondi Beach',
+    text:'Bondi Beach is a popular beach and the name of the surrounding suburb in Sydney, New South Wales, Australia. Bondi Beach is located 7 km (4 mi) east of the Sydney central business district, in the local government area of Waverley Council, in the Eastern Suburbs. It has a population of 11,656 residents. Its post code is 2026. Bondi, North Bondi, and Bondi Junction are neighbouring suburbs. Bondi Beach is one of the most visited tourist sites in Australia'  
   }
 ]
 
